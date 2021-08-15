cf0f1deb4e0f264e74d029ee9c998c1863fda73d
David Wire <dlwire@gmail.com>
2021-08-15 17:08:40 -0600

    finishing out coin support in the evaluator

--- src/coinEvaluator_test.py
+++ src/coinEvaluator_test.py
@@ -4,6 +4,8 @@ from coins import (
     ONE_DOLLAR_COIN,
     HALF_DOLLAR_COIN,
     QUARTER_DOLLAR_COIN,
+    DIME_COIN,
+    NICKEL_COIN,
 )
 
 
@@ -16,6 +18,12 @@ class CoinEvaluatorTest(unittest.TestCase):
 
     def test_QuarterDollarCoin(self):
         self.assertEqual(evaluateCoin(QUARTER_DOLLAR_COIN), 25)
+
+    def test_DimeCoin(self):
+        self.assertEqual(evaluateCoin(DIME_COIN), 10)
+
+    def test_NickelCoin(self):
+        self.assertEqual(evaluateCoin(NICKEL_COIN), 5)
 
     def test_invalidCoin(self):
         self.assertEqual(evaluateCoin(('', 1, 1, 1)), 0)
--- src/coins.py
+++ src/coins.py
@@ -29,9 +29,24 @@ QUARTER_DOLLAR_COIN: Coin = (
     1.75
 )
 
+DIME_COIN: Coin = (
+    'Cupro-Nickel',
+    2.268,
+    17.91,
+    1.35,
+)
+
+NICKEL_COIN: Coin = (
+    'Cupro-Nickel',
+    5.000,
+    21.21,
+    1.95,
+)
 
 COIN_VALUES = {
     ONE_DOLLAR_COIN: 100,
     HALF_DOLLAR_COIN: 50,
     QUARTER_DOLLAR_COIN: 25,
+    DIME_COIN: 10,
+    NICKEL_COIN: 5,
 }
